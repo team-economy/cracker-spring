448a98a535788d42df2359c98497fbd5f10dea13
devpcjin <sgn03077@gmail.com>
2022-06-27 12:33:31 +0900

    Create Project

---
+++ src/main/java/com/cracker/CrackerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrackerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrackerSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cracker/CrackerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrackerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
