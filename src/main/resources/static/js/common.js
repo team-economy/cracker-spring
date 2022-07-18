a52b913d9d7371d27031e6acc7a6347fb1bf599c
shlee <myjade0416@gmail.com>
2022-07-18 16:13:55 +0900

    Delete access_token Cookie when you logout

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -4,6 +4,12 @@
 function sign_out() {
     $.removeCookie('access_token', {path: '/'});
     $.removeCookie('refresh_token', {path: '/'});
-    alert('로그아웃!')
-    window.location.href = "/login"
+    $.ajax({
+        type: "DELETE",
+        url: `/api/cracker/logout`,
+        success: function (response) {
+            alert('로그아웃!');
+            window.location.href = "/login"
+        }
+    });
 }
