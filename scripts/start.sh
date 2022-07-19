b688f84b6a5ee8122e4972c8796b1167efb343e8
devpcjin <sgn03077@gmail.com>
2022-07-20 01:26:09 +0900

    Deploy test

--- scripts/start.sh
+++ scripts/start.sh
@@ -33,4 +33,5 @@ echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
     -Dspring.config.location=classpath:/application-$IDLE_PROFILE.yml \
     -Dspring.profiles.active=$IDLE_PROFILE \
+    -Duser.timezone=Asia/Seoul \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
