884b5e78a51a98b11635a867c2b4ad159c788a8e
shlee <myjade0416@gmail.com>
2022-07-19 15:48:41 +0900

    delete all cookie when logout

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -7,6 +7,7 @@ function sign_out() {
     $.ajax({
         type: "DELETE",
         url: `/api/cracker/logout`,
+        global: false,
         success: function (response) {
             alert('로그아웃!');
             window.location.href = "/login"
