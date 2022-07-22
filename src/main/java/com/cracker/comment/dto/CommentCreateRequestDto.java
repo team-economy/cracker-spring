273223fa86442c54ec70f9f5d1131b7ed8f7971b
shlee <myjade0416@gmail.com>
2022-07-23 02:53:51 +0900

    comment max length

--- src/main/java/com/cracker/comment/dto/CommentCreateRequestDto.java
+++ src/main/java/com/cracker/comment/dto/CommentCreateRequestDto.java
@@ -1,15 +1,14 @@
 package com.cracker.comment.dto;
 
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
-@Getter
+@Data
 @NoArgsConstructor(access = AccessLevel.PROTECTED) //무분별한 객체생성 체크
 public class CommentCreateRequestDto {
 
 //    private String userName;
+    @Length(min=1, max=255)
     private String comment;
 
     private String communityAddr;
