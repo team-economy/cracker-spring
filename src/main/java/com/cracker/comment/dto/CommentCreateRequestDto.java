c77cfbd381581f03de18dad2829fe0711a89e07e
shlee <myjade0416@gmail.com>
2022-07-23 05:03:24 +0900

    test

--- src/main/java/com/cracker/comment/dto/CommentCreateRequestDto.java
+++ src/main/java/com/cracker/comment/dto/CommentCreateRequestDto.java
@@ -1,14 +1,12 @@
 package com.cracker.comment.dto;
 
 import lombok.*;
-import org.hibernate.validator.constraints.Length;
 
 @Data
 @NoArgsConstructor(access = AccessLevel.PROTECTED) //무분별한 객체생성 체크
 public class CommentCreateRequestDto {
 
 //    private String userName;
-//    @Length(min=1, max=255)
     private String comment;
 
     private String communityAddr;
