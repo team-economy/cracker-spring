9578b500b1949c1c5bdf9c92b778bfafdb50533b
devpcjin <sgn03077@gmail.com>
2022-07-22 19:52:08 +0900

    Update IndexController

--- src/main/java/com/cracker/community/repository/CommunityRepository.java
+++ src/main/java/com/cracker/community/repository/CommunityRepository.java
@@ -3,6 +3,6 @@ package com.cracker.community.repository;
 import com.cracker.community.entity.Community;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommunityRepository extends JpaRepository<Community, Object> {
+public interface CommunityRepository extends JpaRepository<Community, Long> {
     Community findByAddr(String addr);
 }
